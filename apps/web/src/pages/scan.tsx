c8662098509ea1e42180f97b111e8a0e170314d1
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-10 12:03:24 +0100

    test: add qr code reader

--- apps/web/src/pages/scan.tsx
+++ apps/web/src/pages/scan.tsx
@@ -1,11 +1,33 @@
 import H1 from "@/ui/typography/H1";
 import { getAuth } from "@clerk/nextjs/server";
 import { GetServerSidePropsContext } from "next";
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
 
 const ScanPage = () => {
+  const [data, setData] = useState("No result");
+
   return (
     <div>
-      <H1>Add sensor</H1>
+      <H1>Add Sensor</H1>
+
+      <div>
+        <QrReader
+          onResult={(result, error) => {
+            if (!!result) {
+              setData(result?.getText());
+            }
+
+            if (!!error) {
+              console.info(error);
+            }
+          }}
+          constraints={{
+            facingMode: "environment",
+          }}
+        />
+        <p>{data}</p>
+      </div>
     </div>
   );
 };
