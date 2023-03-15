728785424e9ec19c0fe5e00e630f5d1d9a0d4037
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-15 14:41:27 +0100

    refactor: make CI pass

--- apps/web/src/utils/sensorDataInRange.ts
+++ apps/web/src/utils/sensorDataInRange.ts
@@ -1,39 +1,51 @@
-export function getScale(
-  arr: number[][],
-  height: number,
-  offset: number,
-): number {
+export const getScale = (arr: number[][], height: number, offset: number) => {
   const medianValue = getMedianValue(arr);
-  // Calculate the maximum value of the scale based on the height and offset
   const maxScaleValue = height - offset;
-  // Calculate the difference between the median value and the offset
-  const diff = Math.abs(medianValue - offset);
-  // Calculate the scale from 0-100
+  const difference = Math.abs(medianValue - offset);
+
+  // Calculate the scale from 0 - 100%
   const scale = Math.max(
     0,
-    Math.min(100, ((maxScaleValue - diff) / maxScaleValue) * 100),
+    Math.min(100, ((maxScaleValue - difference) / maxScaleValue) * 100),
   );
   return scale;
-}
+};
 
-function getMedianValue(arr: number[][]): number {
-  const n = arr.length;
+const arrayIsSquare = (arr: number[][]) => {
+  return arr.length === arr[0]?.length;
+};
+
+const getMedianValue = (array: number[][]) => {
   const middlePoints = [];
+
   // Check if the input array is square
-  if (n !== arr[0].length) {
-    throw new Error("Input array must be square");
+  if (!arrayIsSquare(array)) {
+    throw new Error("Array must be square");
   }
+
   // Get the middle points of the array
-  for (let i = 1; i < n - 1; i++) {
-    for (let j = 1; j < n - 1; j++) {
-      middlePoints.push(arr[i][j]);
+  for (let i = 1; i < array.length - 1; i++) {
+    for (let j = 1; j < array.length - 1; j++) {
+      const row = array[i];
+      if (!row) continue;
+
+      const col = row[j];
+      if (!col) continue;
+
+      middlePoints.push(col);
     }
   }
+
   // Sort the middle points
   middlePoints.sort((a, b) => a - b);
+
   // Get the median value
   const medianIndex = Math.floor(middlePoints.length / 2);
   const medianValue = middlePoints[medianIndex];
 
+  if (!medianValue) {
+    throw new Error("Median value is undefined");
+  }
+
   return medianValue;
-}
+};
