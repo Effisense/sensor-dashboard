a80f9f01066f53a7c4300ce7f49ac0d9292507cf
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-15 14:45:33 +0100

    export function

--- apps/web/src/utils/sensorToFillLevel.ts
+++ apps/web/src/utils/sensorToFillLevel.ts
@@ -1,5 +1,4 @@
-function sensorToFillLevel(sensorValue: number): number {
-
-    //TODO: Implement this function
+export const sensorToFillLevel = (sensorValue: number): number => {
+  // TODO: Implement this function
   return Math.round((sensorValue / 1023) * 100);
-}
+};
