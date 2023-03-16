70412ab306c51e751a77a7bcd51b783ea5fcb9db
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-16 14:59:51 +0100

    simply using the timeseries in context also works

--- packages/api/src/router/timeseries.ts
+++ packages/api/src/router/timeseries.ts
@@ -1,7 +1,10 @@
 import { router, publicProcedure } from "../trpc";
 
 export const timeseriesRouter = router({
-  hello: publicProcedure.query(({ ctx }) => {
+  hello: publicProcedure.query(async ({ ctx }) => {
+    const result = await ctx.timeseries.Sensor.select("battery").execute();
+    console.log(result);
+
     return "hello";
   }),
 });
