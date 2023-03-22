436966798bd1aad032038e45de192ab8cebca96d
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-22 13:02:30 +0100

    feat: CRUD for container type

--- packages/api/src/router/index.ts
+++ packages/api/src/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from "../trpc";
 import { authRouter } from "./auth";
 import { mapRouter } from "./map";
+import { sensorRouter } from "./sensor";
 
 export const appRouter = router({
   auth: authRouter,
   map: mapRouter,
+  sensor: sensorRouter,
 });
 
 // export type definition of API
