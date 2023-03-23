31c306aa155cb7879b4f43c7d73b5bab0e350b97
helenaskants <helenaskants@hotmail.com>
2023-03-23 15:46:07 +0100

    wip

--- apps/web/src/pages/sensors/create.tsx
+++ apps/web/src/pages/sensors/create.tsx
@@ -18,35 +18,35 @@ interface FormData {
   }
 
 const CreateSensorPage = () => {
-  const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
 
   const onSubmit = handleSubmit(data => console.log(data));
 
   return (
-    <><div className="bg-slate-300 min-w-[30%] justify-center align-middle grid rounded-md mt-4 mb-8">
+    <><div className="bg-slate-300 w-[400px] grid rounded-md mt-4 mb-8">
       
-    <div className="mt-2 grid align-middle justify-center">
+      <div className="mt-2 grid align-middle justify-center">
         <H4>Add sensor</H4>
-    </div>
-     
-    <form onSubmit={onSubmit} className="w-[100%] grid gap-x-8 gap-y-4 grid-cols-1 my-4 mb-8 justify-center align-middle">
-      <Label htmlFor="name">Name</Label>
-      <Input className="bg-white"  placeholder="Name" {...register("name")} />
+      </div>
+      
+      <form onSubmit={onSubmit} className="grid gap-y-4 grid-cols-1 my-4 mb-8 align-middle justify-center items-center">
+        <Label htmlFor="name">Name</Label>
+        <Input className="bg-white align-middle justify-center w-3/4"  placeholder="Name" {...register("name")} />
 
-      <Label htmlFor="containerId">Container ID</Label>
-      <Input className="bg-white"  placeholder="Container ID" {...register("containerId")} />
+        <Label htmlFor="containerId">Container ID</Label>
+        <Input className="bg-white w-3/4"  placeholder="Container ID" {...register("containerId")} />
 
-      <Label htmlFor="collectionId">Collection ID</Label>
-      <Input className="bg-white"  placeholder="Collection ID" {...register("collectionId")} />
+        <Label htmlFor="collectionId">Collection ID</Label>
+        <Input className="bg-white w-3/4"  placeholder="Collection ID" {...register("collectionId")} />
 
-      <Label htmlFor="description">Description</Label>
-      <Textarea className="bg-white" placeholder="Description"  {...register("description")}/>
+        <Label htmlFor="description">Description</Label>
+        <Textarea className="bg-white w-3/4" placeholder="Description"  {...register("description")}/>
 
-      <Label htmlFor="nickname">Location</Label>
-      <Map/>
+        <Label htmlFor="nickname">Location</Label>
+        <Map/>
 
-      <Button variant="default" type="button">Add sensor</Button>
-    </form> 
+        <Button variant="default" type="button" className="w-3/4">Add sensor</Button>
+      </form> 
     </div>
       </>
   );
