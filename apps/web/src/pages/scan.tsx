ccb01c451e07da43e7cff69a9d0c666e49b05c9c
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-27 15:59:59 +0200

    clean up scan page

--- apps/web/src/pages/scan.tsx
+++ apps/web/src/pages/scan.tsx
@@ -1,6 +1,7 @@
 import useScanSensor from "@/hooks/useScanSensor";
 import QrReader from "@/ui/QrReader";
 import H1 from "@/ui/typography/H1";
+import Subtle from "@/ui/typography/Subtle";
 import { getAuth } from "@clerk/nextjs/server";
 import { GetServerSidePropsContext } from "next";
 
@@ -8,17 +9,19 @@ const ScanPage = () => {
   const { handleScan } = useScanSensor();
 
   return (
-    <div className="">
-      <H1 className="text-center">Add Sensor</H1>
+    <div className="w-full">
+      <div className="flex flex-col items-center justify-center py-8">
+        <H1>Welcome to Effisense</H1>
+        <Subtle>Please scan the QR code on your sensor.</Subtle>
+      </div>
 
-      <div>
+      <div className="mx-auto w-11/12 md:w-3/4 lg:w-1/2">
         <QrReader
           onResult={(result) => {
             if (!!result) {
               handleScan(result.getText());
             }
           }}
-          className="h-full w-full"
         />
       </div>
     </div>
