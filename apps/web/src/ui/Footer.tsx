d3509aa12b3567852d8de41c637e788d3187169c
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-27 16:02:24 +0200

    style: add footer

--- apps/web/src/ui/Footer.tsx
+++ apps/web/src/ui/Footer.tsx
@@ -1,6 +1,22 @@
+import Link from "next/link";
+import Subtle from "./typography/Subtle";
+
 const Footer = () => {
-  // TODO: Add footer
-  return <div>footer goes here</div>;
+  return (
+    <div className="flex flex-col items-center justify-center py-4">
+      <Subtle>
+        Developed and designed by{" "}
+        <Link
+          href="https://www.jrc.no/"
+          target={"_blank"}
+          rel="noopener noreferrer"
+          className="text-sage-12 hover:text-sage-11 hover:underline"
+        >
+          Junior Consulting
+        </Link>
+      </Subtle>
+    </div>
+  );
 };
 
 export default Footer;
