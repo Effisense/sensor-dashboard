a8356352f4ffc0ddbf45e832547e7fbb983e0b01
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-29 17:36:55 +0200

    feat: finish update container view

--- apps/web/src/pages/containers/[id]/update.tsx
+++ apps/web/src/pages/containers/[id]/update.tsx
@@ -1,11 +1,14 @@
 import useUpdateContainerForm from "@/hooks/forms/useUpdateContainerForm";
+import { useToast } from "@/hooks/useToast";
 import { Button } from "@/ui/Button";
 import FormInput from "@/ui/FormInput";
 import FormTextarea from "@/ui/FormTextarea";
+import LoadingSpinner from "@/ui/LoadingSpinner";
 import H1 from "@/ui/typography/H1";
 import Subtle from "@/ui/typography/Subtle";
 import { trpc } from "@/utils/trpc";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { useEffect } from "react";
 
 type ContainerPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
@@ -18,81 +21,97 @@ const UpdateContainerPage = ({ id }: ContainerPageProps) => {
   const { register, onSubmit, handleSubmit, errors } = useUpdateContainerForm({
     id,
   });
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (error) {
+      toast({
+        title: "Oops!",
+        description: `An error occurred: ${error.message}`,
+        severity: "error",
+      });
+    }
+  }, [error, toast]);
 
   return (
     <div>
-      <H1>{data?.name}</H1>
-      <Subtle>Update information about this container.</Subtle>
+      {isLoading && <LoadingSpinner />}
+      {!error && !isLoading && data && (
+        <div>
+          <H1>{data?.name}</H1>
+          <Subtle>Update information about this container.</Subtle>
 
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="my-8 flex flex-col items-center justify-center gap-y-2"
-      >
-        <FormInput
-          label="Name"
-          errorMessage={errors.name?.message}
-          id="name"
-          register={register}
-          defaultValue={data?.name}
-        />
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="my-8 flex flex-col items-center justify-center gap-y-2"
+          >
+            <FormInput
+              label="Name"
+              errorMessage={errors.name?.message}
+              id="name"
+              register={register}
+              defaultValue={data?.name}
+            />
 
-        <FormTextarea
-          label="Description"
-          errorMessage={errors.description?.message}
-          id="description"
-          register={register}
-          defaultValue={data?.description}
-        />
+            <FormTextarea
+              label="Description"
+              errorMessage={errors.description?.message}
+              id="description"
+              register={register}
+              defaultValue={data?.description}
+            />
 
-        <FormInput
-          label="Target fill level (%)"
-          errorMessage={errors.name?.message}
-          id="targetFillLevelInPercent"
-          register={register}
-          valueAsNumber
-          defaultValue={data?.targetFillLevelInPercent}
-        />
+            <FormInput
+              label="Target fill level (%)"
+              errorMessage={errors.name?.message}
+              id="targetFillLevelInPercent"
+              register={register}
+              valueAsNumber
+              defaultValue={data?.targetFillLevelInPercent}
+            />
 
-        <FormInput
-          register={register}
-          id="binHeightInMillimeters"
-          label="Bin height (mm)"
-          errorMessage={errors.binHeightInMillimeters?.message}
-          valueAsNumber
-          defaultValue={data?.binHeightInMillimeters}
-        />
+            <FormInput
+              register={register}
+              id="binHeightInMillimeters"
+              label="Bin height (mm)"
+              errorMessage={errors.binHeightInMillimeters?.message}
+              valueAsNumber
+              defaultValue={data?.binHeightInMillimeters}
+            />
 
-        <FormInput
-          register={register}
-          id="binWidthInMillimeters"
-          label="Bin width (mm)"
-          errorMessage={errors.binWidthInMillimeters?.message}
-          valueAsNumber
-          defaultValue={data?.binWidthInMillimeters}
-        />
+            <FormInput
+              register={register}
+              id="binWidthInMillimeters"
+              label="Bin width (mm)"
+              errorMessage={errors.binWidthInMillimeters?.message}
+              valueAsNumber
+              defaultValue={data?.binWidthInMillimeters}
+            />
 
-        <FormInput
-          register={register}
-          id="sensorOffsetInMillimeters"
-          label="Sensor offset (mm)"
-          errorMessage={errors.sensorOffsetInMillimeters?.message}
-          valueAsNumber
-          defaultValue={data?.sensorOffsetInMillimeters}
-        />
+            <FormInput
+              register={register}
+              id="sensorOffsetInMillimeters"
+              label="Sensor offset (mm)"
+              errorMessage={errors.sensorOffsetInMillimeters?.message}
+              valueAsNumber
+              defaultValue={data?.sensorOffsetInMillimeters}
+            />
 
-        <FormInput
-          register={register}
-          id="containerVolumeInLiters"
-          label="Container volume (L)"
-          errorMessage={errors.containerVolumeInLiters?.message}
-          valueAsNumber
-          defaultValue={data?.containerVolumeInLiters}
-        />
+            <FormInput
+              register={register}
+              id="containerVolumeInLiters"
+              label="Container volume (L)"
+              errorMessage={errors.containerVolumeInLiters?.message}
+              valueAsNumber
+              defaultValue={data?.containerVolumeInLiters}
+            />
 
-        <Button variant="default" type="submit" className="w-3/4">
-          Update container
-        </Button>
-      </form>
+            <Button variant="default" type="submit" className="w-3/4">
+              Update container
+            </Button>
+          </form>
+        </div>
+      )}
     </div>
   );
 };
