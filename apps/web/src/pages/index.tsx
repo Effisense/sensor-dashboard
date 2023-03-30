b66ed295038ea490ee03922da0b2675d653a9e80
magnusrodseth <magnus.rodseth@gmail.com>
2023-03-30 12:11:48 +0200

    style: give toast better colors and move it to top

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,15 +4,31 @@ import type {
 } from "next";
 import { getAuth } from "@clerk/nextjs/server";
 import SensorPositionMap from "@/ui/Map";
+import { Button } from "@/ui/Button";
+import { useToast } from "@/hooks/useToast";
 
 type IndexPageProps = InferGetServerSidePropsType<typeof getServerSideProps>;
 
 const IndexPage = ({}: IndexPageProps) => {
+  const { toast } = useToast();
   return (
     <div className="container flex flex-col items-center justify-center gap-12 px-4 py-8">
       <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
         Dashboard
       </h1>
+
+      <Button
+        onClick={() => {
+          toast({
+            title: "Test",
+            description: "Test description",
+            severity: "error",
+            duration: 5000,
+          });
+        }}
+      >
+        Trigger toast
+      </Button>
 
       <SensorPositionMap />
     </div>
