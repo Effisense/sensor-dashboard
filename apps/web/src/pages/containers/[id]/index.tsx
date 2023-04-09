8904b9979e043e2887330d1b38a143040f0f4fc8
helenaskants <helenaskants@hotmail.com>
2023-04-09 10:25:52 +0200

    wip

--- apps/web/src/pages/containers/[id]/index.tsx
+++ apps/web/src/pages/containers/[id]/index.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/ui/Button";
 import H1 from "@/ui/typography/H1";
+import Subtle from "@/ui/typography/Subtle";
 import { trpc } from "@/utils/trpc";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import Link from "next/link";
 
 type ContainerPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
@@ -31,14 +34,24 @@ const ContainerPage = ({ id }: ContainerPageProps) => {
   }
 
   if (!container) {
-    return <div>Container not found</div>;
+    return     <div className="flex flex-col items-center justify-center gap-y-4">
+    <H1>Oh no!</H1>
+    <Subtle>
+      It looks like the container you&apos;re looking for doesn&apos;t exist.
+    </Subtle>
+    <Link href="/">
+      <Button variant="subtle">Take me home</Button>
+    </Link>
+  </div>;
   }
 
   return (
     <div>
-      <H1>Container {container.id}</H1>
+      <H1>{container.name}</H1>
+      <p>Height: {container.binHeightInMillimeters}</p>
+      <p>Width: {container.binWidthInMillimeters}</p>
+      <p>Volume: {container.containerVolumeInLiters}</p>
       <p>Description: {container.description}</p>
-      <p>Name: {container.name}</p>
       <p>Sensors:</p>
       <ul>
         {sensors.map((sensor) => (
