2ec944ff754c7b6eed10cfc9f82c1dd5fd63c1d2
helenaskants <helenaskants@hotmail.com>
2023-04-09 11:54:59 +0200

    feat: add delete button + alertdialog

--- apps/web/src/pages/containers/[id]/index.tsx
+++ apps/web/src/pages/containers/[id]/index.tsx
@@ -1,15 +1,30 @@
+import { toast } from "@/hooks/toast/useToast";
 import { Button } from "@/ui/Button";
 import H1 from "@/ui/typography/H1";
 import Subtle from "@/ui/typography/Subtle";
 import { trpc } from "@/utils/trpc";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/ui/AlertDialog"
 
 type ContainerPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
 >;
 
 const ContainerPage = ({ id }: ContainerPageProps) => {
+
+  const router = useRouter();
   const {
     data: container,
     isLoading: containerIsLoading,
@@ -25,13 +40,42 @@ const ContainerPage = ({ id }: ContainerPageProps) => {
     containerId: id,
   });
 
-  if (containerIsLoading || sensorsIsLoading) {
+  const {
+    mutate: deleteContainer,
+    isLoading: deleteContainerIsLoading,
+    error: deleteContainerError,
+  } = trpc.container.delete.useMutation();
+
+
+  if (containerIsLoading || sensorsIsLoading || deleteContainerIsLoading) {
     return <div>Loading...</div>;
   }
 
-  if (containerError || sensorsError) {
+  if (containerError || sensorsError || deleteContainerError) {
     return <div>Error</div>;
   }
+
+  const handleDelete = async () => {
+    try {
+      await deleteContainer({ containerId: id });
+      toast({
+        title: "Success!",
+        description: "Container was deletedy. You will now be redirected to the dashboard",
+        severity: "success",
+      });
+      router.push({
+        pathname: "/",
+      });
+    } catch (error) {
+      // Handle the error, for example:
+      console.error("Failed to delete container:", error);
+      toast({
+        title: "Error!",
+        description: "There was an error while deleting the container",
+        severity: "error",
+      });
+    }
+  };  
 
   if (!container) {
     return     <div className="flex flex-col items-center justify-center gap-y-4">
@@ -40,7 +84,7 @@ const ContainerPage = ({ id }: ContainerPageProps) => {
       It looks like the container you&apos;re looking for doesn&apos;t exist.
     </Subtle>
     <Link href="/">
-      <Button variant="subtle">Take me home</Button>
+      <Button variant="subtle">Return home</Button>
     </Link>
   </div>;
   }
@@ -60,6 +104,23 @@ const ContainerPage = ({ id }: ContainerPageProps) => {
           </li>
         ))}
       </ul>
+      <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="outline">Delete Container</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Once executed, this action cannot be reversed. It will permanently delete the container and remove it from all sensors that are currently using it.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction onClick={handleDelete}>Continue</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
     </div>
   );
 };
