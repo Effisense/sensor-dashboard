a2f8912d3d5f78d5d4cb7ac89f1539700a6ca2d7
helenaskants <helenaskants@hotmail.com>
2023-04-13 17:53:10 +0200

    feat: add style to container view

--- apps/web/src/pages/containers/[id]/index.tsx
+++ apps/web/src/pages/containers/[id]/index.tsx
@@ -1,14 +1,13 @@
 import { Button } from "@/ui/Button";
-import H1 from "@/ui/typography/H1";
 import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
 import Link from "next/link";
-import { Textarea } from "@/ui/Textarea";
-import H4 from "@/ui/typography/H4";
 import Alert from "@/ui/Alert";
 import { PencilIcon, TrashIcon } from "@heroicons/react/24/outline";
 import useGetContainerWithSensors from "@/hooks/queries/useGetContainerWithSensors";
 import LoadingSpinner from "@/ui/LoadingSpinner";
 import AllSensorsMap from "@/ui/map/AllSensorsMap";
+import { Card, Text, Title } from "@tremor/react";
+import { Divider } from "@tremor/react";
 
 type ContainerPageProps = InferGetServerSidePropsType<
   typeof getServerSideProps
@@ -27,62 +26,82 @@ const ContainerPage = ({ id }: ContainerPageProps) => {
     <div>
       {loading && <LoadingSpinner />}
       {!loading && container && (
-        <div className="flex flex-col items-center justify-center">
-          <H1>{container?.name}</H1>
-          <div className="items-left justify-left my-4 flex flex-col">
-            <H4>Height</H4>
-            <p>{container?.binHeightInMillimeters}</p>
-            <H4>Width</H4>
-            <p>{container?.binWidthInMillimeters}</p>
-            <H4>Volume</H4>
-            <p>{container?.containerVolumeInLiters}</p>
-            <H4>Description</H4>
-            <Textarea value={container?.description} disabled />
-            <H4>Sensors:</H4>
-            <ul className="ml-8 list-disc">
-              {sensors &&
-                sensors.map((sensor) => (
-                  <li key={sensor.id}>
-                    <Link
-                      className="hover:underline"
-                      href={`/sensors/${sensor.id}`}
-                    >
-                      {sensor.name}
-                    </Link>
-                  </li>
-                ))}
-            </ul>
-            <div className="h-full min-w-4xl">
+                <><><div className="flex flex-row items-center justify-center gap-x-8">
+          <div className=" mt-8">
             <AllSensorsMap sensors={sensors} />
-            </div>
           </div>
-          <div className="items-left justify-left my-4 flex flex-row gap-x-4">
-            <Link href={`/containers/${id}/update`}>
-              <Button
-                variant="subtle"
-                className="flex items-center justify-center gap-x-2"
-              >
-                <PencilIcon className="w-4" />
-                <span>Update</span>
-              </Button>
-            </Link>
+          <div className="flex-col flex gap-y-4 mt-4">
+          <Card className="max-w-xs mx-auto">
+              <Title>Sensors:</Title>
+          <div className="flex gap-x-2 mt-4">
+              <ul className="ml-8 list-disc">
+                {sensors &&
+                  sensors.map((sensor) => (
+                    <li key={sensor.id}>
+                      <Link
+                        className="hover:underline"
+                        href={`/sensors/${sensor.id}`}
+                      >
+                        {sensor.name}
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
+              </div>
+          </Card>
+            <Card className="max-w-sm mx-auto flex flex-col">
+              <div className="flex-row flex gap-x-2 mb-4">
+                <Title>{container?.name}</Title>
+              </div>
+              <div className="flex-row flex gap-x-36">
+              <Text>Height</Text>
+              <Text>{container.binHeightInMillimeters}</Text>
+              </div>
+              <Divider className="mb-2 mt-2"/>
+              <div className="flex-row flex gap-x-36">
+              <Text>Width</Text>
+              <Text>{container.binWidthInMillimeters}</Text>
+              </div>
+              <Divider className="mb-2 mt-2" />
+              <div className="flex-row flex gap-x-36">
+              <Text>Volume</Text>
+              <Text>{container.containerVolumeInLiters}</Text>
+              </div>
+              <Divider className="mb-2 mt-2"/>
+              <div className="flex-row flex gap-x-24">
+              <Text>Target fill-level</Text>
+              <Text>{container.targetFillLevelInPercent}</Text>
+              </div>
+              <Card className="max-w-xs mx-auto mt-4">
+                <Text>{container.description}</Text>
+              </Card>
+              <div className="items-left justify-left my-4 flex flex-row gap-x-4 mt-4">
+                <Link href={`/sensors/${id}/update`}>
+                  <Button
+                    variant="subtle"
+                    className="flex items-center justify-center gap-x-2"
+                  >
+                    <PencilIcon className="w-4" />
+                    <span>Update</span>
+                  </Button>
+                </Link>
 
-            <Alert
-              title="Are you sure you want to delete the container??"
-              description="The container? will be deleted, and removed from all sensors that are currently using it."
-              trigger={
-                <Button
-                  variant="subtle"
-                  className="flex items-center justify-center gap-x-2"
-                >
-                  <TrashIcon className="w-4" />
-                  <span>Delete</span>
-                </Button>
-              }
-              onDelete={onDelete}
-            />
+                <Alert
+                  title="Are you sure you want to delete the sensor?"
+                  description="The sensor will be permanently deleted."
+                  trigger={<Button
+                    variant="subtle"
+                    className="flex items-center justify-center gap-x-2"
+                  >
+                    <TrashIcon className="w-4" />
+                    <span>Delete</span>
+                  </Button>}
+                  onDelete={onDelete} />
+              </div>
+            </Card>
           </div>
-        </div>
+        </div></><div>
+          </div></>
       )}
     </div>
   );
