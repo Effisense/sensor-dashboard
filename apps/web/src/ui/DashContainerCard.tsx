f193c7547392d8e2e88a3ec9fd884de8adff66d4
Anders Rodem <anders.rodem@gmail.com>
2023-04-14 15:26:45 +0200

    Dash changes

--- apps/web/src/ui/DashContainerCard.tsx
+++ apps/web/src/ui/DashContainerCard.tsx
@@ -2,93 +2,37 @@ import { Container, Sensor } from "@acme/db";
 import { Card, Title, Flex } from "@tremor/react";
 import { DonutChart } from "@tremor/react";
 import { List, ListItem } from "@tremor/react";
+import { MouseEvent } from "react";
 
-const cities = [
-  {
-    name: "New York",
-    sales: 9800,
-  },
-  {
-    name: "London",
-    sales: 4567,
-  },
-  {
-    name: "Hong Kong",
-    sales: 3908,
-  },
-  {
-    name: "San Francisco",
-    sales: 2400,
-  },
-];
+// DashContainerCard component
+interface DashContainerCardProps {
+  container: Container;
+  onSelect: () => void;
+  isSelected: boolean;
+}
 
-const binFullness = [
-  {
-    desc: "Tomme",
-    num: 1,
-  },
-  {
-    desc: "Halvfulle",
-    num: 1,
-  },
-  {
-    desc: "Nesten fulle",
-    num: 1,
-  },
-  {
-    desc: "Fulle",
-    num: 1,
-  },
-];
+function DashContainerCard({
+  container,
+  onSelect,
+  isSelected,
+}: DashContainerCardProps) {
+  const handleClick = (event: MouseEvent) => {
+    event.preventDefault();
+    onSelect();
+  };
 
-const valueFormatter = (number: number) =>
-  `${Intl.NumberFormat("us").format(number).toString()}`;
-
-function DashContainerCard(
-  container: Container,
-  sensors: Sensor[],
-  header: boolean,
-  selected: boolean,
-) {
   return (
-    <a href="">
-      <Card className={`w-full max-w-lg ${selected ? "bg-yellow-50" : ""}`}>
-        {/* If container is null, show header */}
-        {header ? (
-          <Title className="-my-3">
-            {container.id} {container.length} stk
-          </Title>
-        ) : (
-          <Title className="-my-3">
-            Totaloversikt
-            {container.length} 10 stk
-          </Title>
-        )}
-
-        <Flex className="gap-0">
-          <div className="w-48">
-            <DonutChart
-              className="mt-6"
-              data={cities}
-              category="sales"
-              index="name"
-              valueFormatter={valueFormatter}
-              colors={["green", "yellow", "orange", "rose"]}
-            />
-          </div>
-          <div className="w-full pl-4">
-            <List>
-              {binFullness.map((item) => (
-                <ListItem key={item.desc}>
-                  <span>{item.desc}</span>
-                  <span>{item.num} stk</span>
-                </ListItem>
-              ))}
-            </List>
-          </div>
-        </Flex>
-      </Card>
-    </a>
+    <div>
+      <a href="#" onClick={handleClick}>
+        <Card
+          className={`${
+            isSelected ? "bg-gray-200" : "hover:bg-gray-100"
+          } transition-colors`}
+        >
+          <Title>{container.name}</Title>
+        </Card>
+      </a>
+    </div>
   );
 }
 
--- apps/web/src/ui/Dashboard.tsx
+++ apps/web/src/ui/Dashboard.tsx
@@ -4,6 +4,8 @@ import DashSensorCard from "./DashSensorCard";
 import CreateSensorMap from "./Map";
 import { useState, useEffect } from "react";
 import { Sensor } from "@acme/db";
+import { Card, Title } from "@tremor/react";
+import { MouseEvent } from "react";
 
 interface sensorWithFillType {
   sensor: Sensor;
@@ -21,35 +23,73 @@ export default function Dashboard() {
     isLoading: containerIsLoading,
     error: containersError,
   } = trpc.container.getAll.useQuery();
+
   const [currentSensors, setCurrentSensors] = useState<sensorWithFillType[]>(
     [],
+  );
+  const [selectedContainer, setSelectedContainer] = useState<string | null>(
+    null,
   );
 
   useEffect(() => {
     setCurrentSensors(sensorsWithFillLevel || []);
   }, [sensorsWithFillLevel]);
 
+  const handleContainerSelect = (containerId: string | null) => {
+    setSelectedContainer(containerId);
+    if (containerId === null) {
+      setCurrentSensors(sensorsWithFillLevel || []);
+      return;
+    }
+    const selectedSensors = sensorsWithFillLevel?.filter(
+      (sensorWithFill) => sensorWithFill.sensor.containerId === containerId,
+    );
+    setCurrentSensors(selectedSensors || []);
+  };
+
   return (
     <div className="flex w-full flex-col lg:h-[calc(100vh-8rem)] lg:flex-row">
       <div className="order-2 mx-auto overflow-y-auto p-4  lg:order-1 lg:w-1/4 lg:overflow-y-visible">
         <div className="lg:mx-auto">
-          {/* {containers.map((item, index) => (
-            <div key={index} className="mb-4">
-              <DashContainerCard
-                container={item}
-                sensors={sensors}
-                header={false}
-                selected={false}
-              />
-              {index === 0 && (
-                <h1 className="pt-8 text-lg font-bold">Dine søppelbøtter</h1>
-              )}
+          {containers?.length === 0 ? (
+            <div className="flex h-full flex-col items-center justify-center">
+              <p className="text-sm text-gray-500">
+                Du har ingen søppelbøtter registrert
+              </p>
             </div>
-          ))} */}
+          ) : (
+            <div>
+              <h1 className="pt-8 pb-3 text-lg font-bold">Alle søppelbøtter</h1>
+              <p className="-mt-2 text-sm text-gray-500">
+                Du kan filtrere sensorene ved å trykke på en container
+              </p>
+              <a href="#" onClick={() => handleContainerSelect(null)}>
+                <Card
+                  className={`${
+                    selectedContainer == null
+                      ? "bg-lime-900"
+                      : "hover:bg-green-5"
+                  } mt-4 bg-green-3 transition-colors hover:bg-green-5`}
+                >
+                  <Title>Alle containers</Title>
+                </Card>
+              </a>
+
+              <h1 className="pt-8 pb-3 text-lg font-bold">Dine søppelbøtter</h1>
+              {containers?.map((container, index) => (
+                <div key={index} className="mb-4">
+                  <DashContainerCard
+                    container={container}
+                    isSelected={container.id === selectedContainer}
+                    onSelect={() => handleContainerSelect(container.id)}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
       <div className="order-1 flex-grow lg:order-2 lg:w-2/4 ">
-        {/* Replace the following line with your map component */}
         <div className="h-full w-full">
           <CreateSensorMap />
         </div>
@@ -59,7 +99,8 @@ export default function Dashboard() {
           <h1 className="pb-2 text-lg font-bold">Sensorer</h1>
           {currentSensors.length === 0 && (
             <p className="text-gray-500">
-              Ingen sensorer å vise. Legg til sensorer.
+              Ingen sensorer å vise. Legg til sensorer ved å trykke <br />
+              "Add sensor" i menyen.
             </p>
           )}
           {currentSensors.map((item, index) => (
