3f2a43be8cb0ff980e4cf9681efa546ea7741660
Anders Rodem <anders.rodem@gmail.com>
2023-04-24 14:47:43 +0200

    fillLevel-function

--- packages/api/src/utils/sensor.ts
+++ packages/api/src/utils/sensor.ts
@@ -34,9 +34,45 @@ type FillLevelProps = {
   container: Container | null;
 };
 
-export const getFillLevel = ({ timeseries, container }: FillLevelProps) => {
+export const getFillLevel = ({
+  timeseries,
+  container,
+}: FillLevelProps): number | null => {
   if (!timeseries || !container) return null;
-  // TODO
 
-  return 87;
+  const middlePoints: (number | null)[] = [
+    timeseries.status_z5,
+    timeseries.status_z6,
+    timeseries.status_z9,
+    timeseries.status_z10,
+  ];
+
+  const validPoints: number[] = middlePoints.filter(
+    (point): point is number => point !== null,
+  );
+
+  if (validPoints.length === 0) return null;
+
+  validPoints.sort((a, b) => a - b);
+
+  const median: number | undefined =
+    validPoints.length % 2 === 0
+      ? (validPoints[validPoints.length / 2 - 1] +
+          validPoints[validPoints.length / 2]) /
+        2
+      : validPoints[Math.floor(validPoints.length / 2)];
+
+  if (median === undefined) return null;
+
+  const sensorOffset = container.sensorOffsetInMillimeters ?? 0;
+  const binHeight = container.binHeightInMillimeters;
+
+  if (median < sensorOffset) {
+    return 100;
+  }
+
+  const distanceFromTop = median - sensorOffset;
+  const fullness = ((binHeight - distanceFromTop) / binHeight) * 100;
+
+  return Math.min(Math.max(0, fullness), 100);
 };
