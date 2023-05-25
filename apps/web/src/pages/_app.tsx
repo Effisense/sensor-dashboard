a9da56ba5b2a63fdc05c2f41c12ca6dde1ce2dde
magnusrodseth <magnus.rodseth@gmail.com>
2023-05-25 10:01:23 +0200

    extract clerk stuff to its own provider

--- apps/web/src/pages/_app.tsx
+++ apps/web/src/pages/_app.tsx
@@ -3,42 +3,14 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 
 import type { AppType } from "next/app";
-import { ClerkProvider } from "@clerk/nextjs";
 import { trpc } from "../utils/trpc";
 import Layout from "../ui/Layout";
 import { Toaster } from "@/ui/Toaster";
-import { emerald, yellow, red, slate } from "tailwindcss/colors";
+import ClerkProvider from "@/ui/providers/ClerkProvider";
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
-    <ClerkProvider
-      {...pageProps}
-      appearance={{
-        variables: {
-          colorPrimary: slate[500],
-          colorBackground: slate[50],
-          colorDanger: red[500],
-          colorSuccess: emerald[500],
-          colorWarning: yellow[500],
-          colorText: slate[900],
-        },
-        signIn: {
-          variables: {
-            colorPrimary: emerald[600],
-          },
-        },
-        signUp: {
-          variables: {
-            colorPrimary: emerald[600],
-          },
-        },
-        organizationProfile: {
-          variables: {
-            colorPrimary: emerald[600],
-          },
-        },
-      }}
-    >
+    <ClerkProvider pageProps={pageProps}>
       <Layout>
         <Component {...pageProps} />
         {/* Load `Toaster` at top-level, and render it using `useToast` */}
---
+++ apps/web/src/ui/providers/ClerkProvider.tsx
@@ -0,0 +1,49 @@
+import React, { ReactNode } from "react";
+import { ClerkProvider as _ClerkProvider } from "@clerk/nextjs";
+import { slate, red, emerald, yellow } from "tailwindcss/colors";
+
+interface ClerkProviderProps {
+  children: ReactNode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  pageProps: any;
+}
+
+const ClerkProvider: React.FC<ClerkProviderProps> = ({
+  children,
+  pageProps,
+}) => {
+  return (
+    <_ClerkProvider
+      {...pageProps}
+      appearance={{
+        variables: {
+          colorPrimary: slate[500],
+          colorBackground: slate[50],
+          colorDanger: red[500],
+          colorSuccess: emerald[500],
+          colorWarning: yellow[500],
+          colorText: slate[900],
+        },
+        signIn: {
+          variables: {
+            colorPrimary: emerald[600],
+          },
+        },
+        signUp: {
+          variables: {
+            colorPrimary: emerald[600],
+          },
+        },
+        organizationProfile: {
+          variables: {
+            colorPrimary: emerald[600],
+          },
+        },
+      }}
+    >
+      {children}
+    </_ClerkProvider>
+  );
+};
+
+export default ClerkProvider;
