30e0f4e5bc72716e0537e6e1e6bf990329db67c8
helenaskants <helenaskants@hotmail.com>
2023-05-25 14:46:11 +0200

    Feat: responsive page fix

--- apps/web/src/pages/sensors/[id]/index.tsx
+++ apps/web/src/pages/sensors/[id]/index.tsx
@@ -65,151 +65,146 @@ const SensorPage = ({ id }: SensorPageProps) => {
   }, [fillLevelBetweenDates]);
 
   return (
-    <div className="grid min-h-[calc(100vh-6rem)] w-11/12 grid-cols-1 gap-y-4 md:w-full md:gap-x-2 md:px-4 lg:grid-cols-3 lg:gap-y-0">
-      {isLoading ? (
-        <div className="col-span-3 flex items-center justify-center">
-          <LoadingSpinner />
-        </div>
-      ) : (
-        data && (
-          <>
-            <div
-              className={cn(
-                "col-span-1 grid w-full rounded-lg lg:col-span-2",
-                "bg-slate-50 shadow-md transition-all duration-300 md:m-0",
-                "flex flex-col items-center justify-start p-4",
-                "md:h-auto",
-                "h-1/2",
-              )}
-            >
-              {sensorWithFillLevel ? (
-                <AllSensorsMap sensorsWithFillLevel={[sensorWithFillLevel]} />
-              ) : (
-                <div className="flex h-full w-full items-center justify-center">
-                  <LoadingSpinner />
-                </div>
-              )}
-            </div>
-
-            <div className="col-span-1 h-96 lg:col-span-1 lg:h-auto">
-              <div className="mb-4 mt-4 flex flex-col items-center justify-start">
-                <div className="flex flex-row gap-x-2">
-                  <H3>{data.sensor.name}</H3>
-                </div>
-                <Subtle>Information about your sensor</Subtle>
+  <><div className="visible lg:invisible lg:mb-0 mb-4 text-center">
+      <Subtle>Information about your sensor</Subtle>
+    </div>
+    <div className="grid min-h-[calc(100vh-6rem)] w-11/12 grid-cols-1 md:w-full md:gap-x-2 md:px-4 lg:grid-cols-3">
+        {isLoading ? (
+          <div className="col-span-3 flex items-center justify-center">
+            <LoadingSpinner />
+          </div>
+        ) : (
+          data && (
+            <>
+              <div
+                className={cn(
+                  "col-span-1 grid w-full rounded-lg lg:col-span-2",
+                  "bg-slate-50 shadow-md transition-all duration-300 lg:p-4 p-0",
+                  "flex flex-col items-center justify-start",
+                  "h-[calc(100vh-12rem)] lg:h-auto lg:mt-2"
+                )}
+              >
+                {sensorWithFillLevel ? (
+                  <>
+                    <AllSensorsMap sensorsWithFillLevel={[sensorWithFillLevel]} /></>
+                ) : (
+                  <div className="flex h-full w-full items-center justify-center">
+                    <LoadingSpinner />
+                  </div>
+                )}
               </div>
-              <div className="my-4 flex items-center justify-center gap-x-4">
-                <Link href={`/sensors/${id}/update`}>
-                  <Button
-                    variant="subtle"
-                    className="flex items-center justify-center gap-x-2"
-                  >
-                    <PencilIcon className="w-4" />
-                    <span>Update</span>
-                  </Button>
-                </Link>
-                <Alert
-                  title="Are you sure you want to delete the sensor?"
-                  description="The sensor will be permanently deleted."
-                  trigger={
+              <div className="col-span-1 lg:col-span-1 lg:h-auto">
+                <div className="mb-4 lg:mt-4 mt-0 flex flex-col items-center justify-start">
+                  <div className="flex flex-row gap-x-2 mt-6 lg:mt-0">
+                    <H3>{data.sensor.name}</H3>
+                  </div>
+                  <Subtle className="invisible lg:visible">Information about your sensor</Subtle>
+                </div>
+                <div className="my-4 flex items-center justify-center gap-x-4">
+                  <Link href={`/sensors/${id}/update`}>
                     <Button
+                      variant="subtle"
+                      className="flex items-center justify-center gap-x-2"
+                    >
+                      <PencilIcon className="w-4" />
+                      <span>Update</span>
+                    </Button>
+                  </Link>
+                  <Alert
+                    title="Are you sure you want to delete the sensor?"
+                    description="The sensor will be permanently deleted."
+                    trigger={<Button
                       variant="subtle"
                       className={cn(
                         "flex items-center justify-center gap-x-2 bg-red-500 text-slate-100",
-                        "hover:bg-red-600 hover:text-slate-50",
+                        "hover:bg-red-600 hover:text-slate-50"
                       )}
                     >
                       <TrashIcon className="w-4" />
                       <span>Delete</span>
-                    </Button>
-                  }
-                  onDelete={onDelete}
-                />
-              </div>
-              <div className="mx-auto max-w-md">
-                <Card
-                  className="mx-auto flex max-w-md flex-col gap-y-4"
-                  decoration="top"
-                  decorationColor="teal"
-                >
-                  <div className="flex h-8 items-center">
-                    <Badge
-                      size="sm"
-                      color={
-                        sensorWithFillLevel && sensorWithFillLevel
+                    </Button>}
+                    onDelete={onDelete} />
+                </div>
+                <div className="mx-auto max-w-md">
+                  <Card
+                    className="mx-auto flex max-w-md flex-col gap-y-4"
+                    decoration="top"
+                    decorationColor="teal"
+                  >
+                    <div className="flex h-8 items-center">
+                      <Badge
+                        size="sm"
+                        color={sensorWithFillLevel && sensorWithFillLevel
                           ? percentToColorTremor(sensorWithFillLevel.fillLevel)
-                          : "gray"
-                      }
-                      className="mr-4 py-1 px-2"
-                    >
-                      {!sensorWithFillLevel?.fillLevel
-                        ? "N/A"
-                        : `${sensorWithFillLevel?.fillLevel} %`}
-                    </Badge>
-                  </div>
-                  <div className="mt-2 flex flex-row gap-x-2">
-                    <TrashIcon className="w-4" />
-                    <Text>
-                      {data?.container ? (
-                        <>
-                          <Link
-                            className="hover:underline"
-                            href={`/containers/${data.sensor.containerId}`}
-                          >
-                            {data.container.name}
-                          </Link>
-                        </>
-                      ) : (
-                        <span>No container</span>
-                      )}
-                    </Text>
-                  </div>
-                  <div className="flex flex-row gap-x-2">
-                    <MapPinIcon className="w-4" />
-                    <Text>{data?.sensor.location}</Text>
-                  </div>
-                  <Card className="mt-1 w-full p-4">
-                    <Text>{data?.sensor.description}</Text>
-                  </Card>
-                </Card>
-                <Title className="pt-5">History</Title>
-                <DateRangePicker
-                  className="mx-auto max-w-md pt-4"
-                  value={[startDate, endDate]}
-                  onValueChange={setDateRange}
-                  dropdownPlaceholder="Select dates"
-                />
-                {fillLevelBetweenDatesIsLoading ? (
-                  <div className="flex h-72 items-center justify-center">
-                    <LoadingSpinner />
-                  </div>
-                ) : (
-                  chartData.length === 0 && (
-                    <div className="flex h-72 items-center justify-center">
-                      <p className="px-12 text-center text-sm text-gray-500">
-                        No data available. Choose another time interval or wait
-                        for sensor to collect data.
-                      </p>
+                          : "gray"}
+                        className="mr-4 py-1 px-2"
+                      >
+                        {!sensorWithFillLevel?.fillLevel
+                          ? "N/A"
+                          : `${sensorWithFillLevel?.fillLevel} %`}
+                      </Badge>
                     </div>
-                  )
-                )}
+                    <div className="mt-2 flex flex-row gap-x-2">
+                      <TrashIcon className="w-4" />
+                      <Text>
+                        {data?.container ? (
+                          <>
+                            <Link
+                              className="hover:underline"
+                              href={`/containers/${data.sensor.containerId}`}
+                            >
+                              {data.container.name}
+                            </Link>
+                          </>
+                        ) : (
+                          <span>No container</span>
+                        )}
+                      </Text>
+                    </div>
+                    <div className="flex flex-row gap-x-2">
+                      <MapPinIcon className="w-4" />
+                      <Text>{data?.sensor.location}</Text>
+                    </div>
+                    <Card className="mt-1 w-full p-4">
+                      <Text>{data?.sensor.description}</Text>
+                    </Card>
+                  </Card>
+                  <Title className="pt-5">History</Title>
+                  <DateRangePicker
+                    className="mx-auto max-w-md pt-4"
+                    value={[startDate, endDate]}
+                    onValueChange={setDateRange}
+                    dropdownPlaceholder="Select dates" />
+                  {fillLevelBetweenDatesIsLoading ? (
+                    <div className="flex h-72 items-center justify-center">
+                      <LoadingSpinner />
+                    </div>
+                  ) : (
+                    chartData.length === 0 && (
+                      <div className="flex h-72 items-center justify-center">
+                        <p className="px-12 text-center text-sm text-gray-500">
+                          No data available. Choose another time interval or wait
+                          for sensor to collect data.
+                        </p>
+                      </div>
+                    )
+                  )}
 
-                {chartData.length > 0 && (
-                  <AreaChart
-                    className="mt-4 h-72"
-                    data={chartData}
-                    index="date"
-                    categories={[FILL_LEVEL_LEGEND]}
-                    colors={["emerald"]}
-                    valueFormatter={formatAreaChart}
-                  />
-                )}
+                  {chartData.length > 0 && (
+                    <AreaChart
+                      className="mt-4 h-72"
+                      data={chartData}
+                      index="date"
+                      categories={[FILL_LEVEL_LEGEND]}
+                      colors={["emerald"]}
+                      valueFormatter={formatAreaChart} />
+                  )}
+                </div>
               </div>
-            </div>
-          </>
-        )
-      )}
-    </div>
+            </>
+          )
+        )}
+      </div></>
   );
 };
 
