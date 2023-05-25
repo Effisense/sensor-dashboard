6261ef0222e1736f6b703487bd48ed5f7bed001c
magnusrodseth <magnus.rodseth@gmail.com>
2023-05-25 09:33:25 +0200

    create sensor page

--- apps/web/src/pages/sensors/create.tsx
+++ apps/web/src/pages/sensors/create.tsx
@@ -48,26 +48,28 @@ const CreateSensorPage = ({
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="absolute top-28 left-0 mx-8 mt-4">
-              <Subtle>Please set the position of the sensor</Subtle>
-          </div>
-          <div className="flex w-screen justify-center lg:mt-0">
-              <H2>Add sensor</H2>
-          </div>
-          <div className={cn(
-            "md:grid",
-            "flex min-h-[calc(100vh-12rem)]",
-            "w-full gap-y-4 lg:grid-cols-3 lg:gap-y-0",
-            "w-screen p-6 flex-col gap-y-20"
-          )}>        
+      <div className="flex w-screen justify-center lg:mt-0">
+        <H2>Add sensor</H2>
+      </div>
+      <div
+        className={cn(
+          "md:grid",
+          "flex min-h-[calc(100vh-12rem)]",
+          "w-full gap-y-4 lg:grid-cols-3 lg:gap-y-0",
+          "w-screen flex-col gap-y-20 p-6",
+        )}
+      >
         <div
           className={cn(
-            "col-span-1 grid w-full rounded-lg lg:col-span-2",
-            "bg-slate-50 shadow-md transition-all duration-300 mt-4",
-            "flex flex-row md:order-1 sm:order-1 lg:order:1",
-            "h-[calc(100vh-3rem)] lg:h-[365px]"
+            "col-span-1 grid w-full lg:col-span-2",
+            "transition-all duration-300",
+            "lg:order:1 flex flex-col items-start justify-center sm:order-1 md:order-1",
+            "sm:h-[calc(100vh-12rem)] lg:h-auto",
           )}
         >
+          <Subtle className="w-full py-2 text-center">
+            Please set the position of the sensor.
+          </Subtle>
           <SetSensorPositionMap
             container={container}
             location={location}
@@ -75,9 +77,8 @@ const CreateSensorPage = ({
             isLoading={mapIsLoading}
           />
         </div>
-        <div className="col-span-1 h-80 lg:col-span-1 lg:h-auto lg:w-96 flex justify-center items-center flex-col md:order-2 sm:order-2 lg:order-2">
-          <div className="flex flex-col items-center justify-center">
-            <div className="w-64 px-4">
+        <div className="col-span-1 flex w-full flex-col items-center justify-center sm:order-2 md:order-2 md:mt-8 lg:order-2 lg:col-span-1 lg:mt-0 lg:h-auto">
+          <div>
             <FormInput
               label="Name"
               errorMessage={errors.name?.message}
@@ -92,19 +93,14 @@ const CreateSensorPage = ({
               label="Description"
             />
           </div>
-            <SelectContainerDropdown
-              containerId={containerId}
-              setContainerId={setContainerId}
-              data={data}
-              isLoading={isLoading}
-            />
-          </div>
+          <SelectContainerDropdown
+            containerId={containerId}
+            setContainerId={setContainerId}
+            data={data}
+            isLoading={isLoading}
+          />
           <div className="flex items-center justify-center">
-            <Button
-              variant="default"
-              type="submit"
-              className="mt-8 w-40"
-            >
+            <Button variant="default" type="submit" className="mt-8 w-40">
               Add sensor
             </Button>
           </div>
