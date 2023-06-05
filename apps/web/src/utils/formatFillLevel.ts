6caab26730bed5e8bbfc673ed4b251ab2b19ace3
magnusrodseth <magnus.rodseth@gmail.com>
2023-06-05 16:36:25 +0200

    fix

--- apps/web/src/utils/formatFillLevel.ts
+++ apps/web/src/utils/formatFillLevel.ts
@@ -1,6 +1,6 @@
 const formatFillLevel = (fillLevel?: number | null): "N/A" | string => {
   if (!fillLevel) return "N/A";
-  return `${fillLevel} %`;
+  return `${fillLevel.toFixed(1)} %`;
 };
 
 export default formatFillLevel;
