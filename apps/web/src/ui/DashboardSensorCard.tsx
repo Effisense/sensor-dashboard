ca1d2f50bad077a08b6ffbeb37b2ca7cf3bd359c
magnusrodseth <magnus.rodseth@gmail.com>
2023-06-24 17:46:02 +0200

    fix filtering in dashboard

--- apps/web/src/ui/DashboardSensorCard.tsx
+++ apps/web/src/ui/DashboardSensorCard.tsx
@@ -1,11 +1,12 @@
 import { Sensor } from "@acme/db";
 import { ArrowRightIcon } from "@heroicons/react/24/outline";
-import { Card, Title, Badge } from "@tremor/react";
+import { Card, Title } from "@tremor/react";
 import Link from "next/link";
 import Subtle from "./typography/Subtle";
 import P from "./typography/P";
 import percentToColorTremor from "@/utils/percentToColor";
 import formatFillLevel from "@/utils/formatFillLevel";
+import { Badge } from "./badge";
 
 type DashboardSensorCardProps = {
   sensor: Sensor;
@@ -21,7 +22,7 @@ const DashboardSensorCard = ({
       <Card className="flex w-full items-center justify-between p-4 hover:bg-slate-50">
         <div className="flex items-center justify-center gap-x-2">
           <Badge
-            size="sm"
+            variant="secondary"
             color={percentToColorTremor(fillLevel)}
             className="mr-4 py-1 px-2"
           >
