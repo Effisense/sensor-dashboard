e1c10da48eb67708ffe95f7339bfe9d2fe67ad0c
magnusrodseth <magnus.rodseth@gmail.com>
2023-06-25 17:06:00 +0200

    remove todos

--- packages/leaflet/index.ts
+++ packages/leaflet/index.ts
@@ -1,11 +1,3 @@
-// TODO
-// - [ ] Add types
-// - [ ] Geocoding
-// - [ ] Facilitate map
-// - [ ] Facilitate marker
-// - [ ] Facilitate popup / tooltip
-// - [ ] Facilitate getLocationFromLngLat
-
 import * as Leaflet from "leaflet";
 import * as ReactLeaflet from "react-leaflet";
 import * as Components from "./src/components";
