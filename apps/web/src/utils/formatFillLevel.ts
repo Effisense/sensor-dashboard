4ad86a0a7302d3ccb413e7a6971330fed7b8633f
magnusrodseth <magnus.rodseth@gmail.com>
2023-08-23 12:59:43 +0200

    display integer value for fill level

--- apps/web/src/utils/formatFillLevel.ts
+++ apps/web/src/utils/formatFillLevel.ts
@@ -1,6 +1,6 @@
 const formatFillLevel = (fillLevel?: number | null): "N/A" | string => {
   if (fillLevel === undefined || fillLevel === null) return "N/A";
-  return `${fillLevel.toFixed(1)} %`;
+  return `${fillLevel.toFixed(0)} %`;
 };
 
 export default formatFillLevel;
